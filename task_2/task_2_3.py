b0bdff5e346f21b581fa80000420c80c9eb6979f
p.teperik <p.teperik@yandex.ru>
2024-01-18 10:47:37 +0300

    Задачи к уроку 2.

--- task_2/task_2_3.py
+++ task_2/task_2_3.py
@@ -1,8 +1,8 @@
 # Задача 2-3
 import math
 fact = 1                                            # факториал 0 принимаем за 1
-#num = int(input('Введите число:'))
-num = 17
+num = int(input('Введите число:'))
+#num = 17
 for x in range(1, num+1):
     fact = fact*(x)
 print(f'Факториал для {num}: {fact}')
