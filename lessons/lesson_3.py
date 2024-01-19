0792358c2fbee20a20ec2cace2a6786c4fc2c159
paulmouse <p.teperik@yandex.ru>
2024-01-19 21:36:58 +0300

    20240119_2136

--- lessons/lesson_3.py
+++ lessons/lesson_3.py
@@ -26,3 +26,65 @@
 # for i in (lst):
 #     lst2.append(sum(lst[0:i+1]))
 # print(lst2)
+
+# lst = [[1,2,3], [10,20], [100, 200]]
+# total = 0
+# for subLst in lst:
+#     print(subLst)
+#     for i in subLst:
+#         total += i
+# print(total)
+
+
+# lst = [[1,2,3], [10,20], [100, 200]]
+# res = []
+# total = 0
+# for subLst in lst:
+#     res += subLst
+#     for i in res:
+#         total = sum(res[0:i+1])
+# print(res)
+# print(total)
+
+# a = 'Hello world'
+# for i in range(len(a)):
+#     print(i, a[i])
+# # print(len(a))
+
+# string1 = 'abccba'
+# string2 = string1[::-1]
+# if string1 == string2:
+#     print('True')
+# else:
+#     print('False')
+
+# string1 = 'abccba'
+# for i in range(len(string1)):
+#     if string1[i] != string1[-1-i]:
+#         print(False)
+#         break
+# else: True
+
+# lst = [1,5,7,89,99]
+#
+# print(lst.join('2'))
+
+
+# alphabet = [chr(a) for a in range(97, 123)]
+# lst = []
+# for i in range(len(alphabet)):
+#     lst.append(alphabet[i] * (i+1))
+# print(lst)
+
+x = 5
+
+while x < 10:
+    print (x)
+    x+= 1
+else:
+    print('Else')
+
+
+
+
+
