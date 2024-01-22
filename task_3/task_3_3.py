45b2a0a72ba0fcbdd716772fac198cf6bf854ece
p.teperik <p.teperik@yandex.ru>
2024-01-22 09:11:11 +0300

    Задачи к уроку 2.

--- task_3/task_3_3.py
+++ task_3/task_3_3.py
@@ -1,11 +1,13 @@
 # На вход подаётся предложение из нескольких слов
 
-str1 = 'На вход подаётся предложение'
+str1 = 'На вход подаётся предложение слово буква поливинилхлорид'
 
 lst1 = str1.split()
-max = 0
+print(max(lst1, key=len), len(max(lst1, key=len)))
 
-for i in lst1:
-    if len(i) > max:
-        max = len(i)
-print(i, max)
+# max = 0
+#
+# for i in lst1:
+#     if len(i) > max:
+#         max = len(i)
+# print(i, max)
