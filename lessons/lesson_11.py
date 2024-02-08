c20e10fc35a407b7eb50797151c31975b8301e4d
p.teperik <p.teperik@yandex.ru>
2024-02-08 16:54:12 +0300

    Урок 11

---
+++ lessons/lesson_11.py
@@ -0,0 +1,13 @@
+# import time #Удобен для технической работы со временем.
+# t0 = time.time()
+# t1 = time.time()
+# print(t1-t0,t0,t1)# Выполните эти команды
+#
+# x =100000
+# for i in range(0, 10):
+#     t0 = time.time()
+#     su = 0
+#     for j in range(i * x):
+#         su+= j
+#     t1 = time.time()
+#     print(i , t1-t0)
---
+++ task_11/task_11_2.py
@@ -0,0 +1,49 @@
+# Задача 11-2
+#
+# Дан файл с расширением .txt , содержащий в каждой строчке следующую информацию:
+# номер, фамилия, имя, компания, зарплата, разделенные запятыми.
+# Создайте Эксельный файл, в который перенесите эту информацию,
+# предварительно отсортировав этот список по компании, по фамилии и по имени
+# В конце списка добавьте строку: ИТОГО и суммарное значение всех зарплат.
+
+import openpyxl
+from openpyxl import Workbook
+wb = Workbook()
+
+wb = openpyxl.load_workbook('../files/empData.xlsx')
+# print(wb.sheetnames)    # имена листов в книге
+ws = wb.active
+
+
+with open('../files/empData.txt','r', encoding='UTF-8') as f:
+    lines = f.readlines()
+    #sortLines = sorted(lines, key=lambda x: (x.split(',')[3]))
+    # sortLines = sorted(lines, key=lambda x: (x.split(',')[1], x.split(',')[2], x.split(',')[3]))
+    sortLines = sorted(lines, key=lambda x: (x.split(',')[3], x.split(',')[1], x.split(',')[2]))
+    print(sortLines)
+
+for i in sortLines:
+    row = i.strip().split(',')
+    print(row)
+    #print(row[-1])
+    # salSum = 0
+    # salVal = int((row[-1]))
+    # salSum += salVal[i]
+    # print(salVal)
+    # for j in row:
+    #      salSum = sum(int(row[-1]))
+    #      print(salSum)
+    ws.append(row)
+# salSum = sum(salVal)
+# print(salSum)
+wb.save('../files/empData.xlsx')
+
+
+
+
+    # for i in f:
+    #     w = ' '.join(sorted(i.split(',') ,key = i[3]))
+    #     w = ' '.join((i.split(',')))
+    #     #w = ((i.split(',')))
+    #     print(w)
+    #         # print(w, file=f2, end='\n')
