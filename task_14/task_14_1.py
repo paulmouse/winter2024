d5268fab52935e6de52aa07a8c07ef65e3436d3b
p.teperik <p.teperik@yandex.ru>
2024-02-16 12:30:21 +0300

    Урок 14

--- task_14/task_14_1.py
+++ task_14/task_14_1.py
@@ -3,18 +3,36 @@
 # цифр введенного целого числа n (n >= 0).
 
 def digitCount(s, c):
-    if len(s) > 0:
-        s = s[:-1]
+    if s > 0:
+        s = int(s / 10)
         # print(s)
         c += 1
         # print('c:',s, c)
-    if len(s) > 0:
-        c = digitCount(s,c)
+    if s > 0:
+        c = digitCount(s, c)
         return c
     else:
         return c
 c = 0
-print('return:', digitCount(str(123123123123), c))
+print('return:', digitCount(123123123123, c))
+
+# def digitCount(s, c):
+#     if len(s) > 0:
+#         s = s[:-1]
+#         # print(s)
+#         c += 1
+#         # print('c:',s, c)
+#     if len(s) > 0:
+#         c = digitCount(s,c)
+#         return c
+#     else:
+#         return c
+# c = 0
+# print('return:', digitCount(str(123123123123), c))
+
+
+
+
 
 # решение без рекурсии
 # def digitCount(n):
