6566aa7db0db51da2cf8d1c8b1c5cfc011668d5c
p.teperik <p.teperik@yandex.ru>
2024-02-19 13:34:48 +0300

    Урок 15

--- task_15/task_15_1.py
+++ task_15/task_15_1.py
@@ -8,3 +8,17 @@
 # ключем x.
 # Например, для x = 1
 # [1, 111, 2222, 11]
+
+
+def numbers(dct, x):
+    result = []
+    for key, value in dct.items():
+        if key == x:
+            result.append(value)
+        elif type(value) == dict:
+            result += numbers(value, x)
+    return result
+
+dct = {1: 1, 2: 2, 3: {2: 22, 3: {1: 111, 2: 222, 3: {0: 1111, 1: 2222, 2: 3333}}, 1: 11}, 6: 22}
+x = 1
+print(numbers(dct, x))
