66809bfaccbe6c6ddc46825e5ca231d61381a286
p.teperik <p.teperik@yandex.ru>
2024-02-19 14:38:21 +0300

    Урок 15

--- task_15/task_15_1.py
+++ task_15/task_15_1.py
@@ -13,12 +13,13 @@
 def numbers(dct, x):
     result = []
     for key, value in dct.items():
-        if key == x:
+        if key == x and type(value) != dict:
             result.append(value)
         elif type(value) == dict:
             result += numbers(value, x)
     return result
 
-dct = {1: 1, 2: 2, 3: {2: 22, 3: {1: 111, 2: 222, 3: {0: 1111, 1: 2222, 2: 3333}}, 1: 11}, 6: 22}
+#dct = {1: 1, 2: 2, 3: {2: 22, 3: {1: 111, 2: 222, 3: {0: 1111, 1: 2222, 2: 3333}}, 1: 11}, 6: 22}
+dct = {1:{1:{1:111}}}
 x = 1
 print(numbers(dct, x))
--- task_15/task_15_2.py
+++ task_15/task_15_2.py
@@ -6,12 +6,12 @@
 # где L – буквы, совпадающие по начертанию в русском и латинском
 # алфавите, D – цифры от 0 до 9.
 # Например, A123BC78 или X666XX178
-#А, В, Е, К, М, Н, О, Р, С, Т, У и Х кириллица
+#АВЕКМНОРСТУХ кириллица
 # A, B, E, K, M, H, O, P, C, T, Y, X
 import re
 # inputString = 'H267BE178 M439KX177 X230EU78 C4286O178 Y349AY123 H494BX78'
-inputString = 'H267BE178 M439KX177 H233TC178 X230EU78 C4286O178 Y349AY123 H494BX78'
+inputString = 'H267BE178 M439KX177 H233TC178 X230EU78 C4286O178 Y349AY123 H494BX78 A123AA78 А123АА78'
 # regex = r'\b\w{1}[ABEKMHOPCTYX]\d{3}\w{2}[ABEKMHOPCTYX]\d{2,3}[178]\b'
-regex = r'\b[ABEKMHOPCTYX][0-9]{3}[ABEKMHOPCTYX]{2}178\b|\b[ABEKMHOPCTYX][0-9]{3}[ABEKMHOPCTYX]{2}78\b'
+regex = r'\b[ABEKMHOPCTYXАВЕКМНОРСТУХ][0-9]{3}[ABEKMHOPCTYXАВЕКМНОРСТУХ]{2}178\b|\b[ABEKMHOPCTYXАВЕКМНОРСТУХ][0-9]{3}[ABEKMHOPCTYXАВЕКМНОРСТУХ]{2}78\b'
 
 print(re.findall(regex,inputString))
