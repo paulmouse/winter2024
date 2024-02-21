3da4bb02f247b34d1fe89e506746690ef707cec0
paulmouse <p.teperik@yandex.ru>
2024-02-21 19:15:41 +0300

    Урок 16

---
+++ lessons/lesson_17.py
@@ -0,0 +1,5 @@
+# import re
+# print(re.findall(r'<.*>', '<first> <second> <third>')) # Жадный поиск
+# print(re.findall(r'<.*?>', '<first> <second> <third>')) # Ленивый поиск
+# print(re.findall(r'a.*a', 'abracadabra')) # Жадный поиск
+# print(re.findall(r'a.*?a', 'abracadabra')) # Ленивый поиск
