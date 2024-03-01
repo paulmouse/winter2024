c9f067bf7dc887d33cf044c5a415073136deb4e3
paulmouse <p.teperik@yandex.ru>
2024-03-01 14:29:19 +0300

    Урок 19

--- task_19/task_19_1.py
+++ task_19/task_19_1.py
@@ -1,23 +1,40 @@
 # 10, 50, 100, 200, 500, 1000, 2000, 5000
+# import itertools
+#
+#
+# def sumFromListCombination():
+#     lst = [10, 50, 100, 200, 500, 1000, 2000, 5000]
+#     # lstlen = len(lst)
+#     sumlst = []
+#     allSums = itertools.chain((itertools.combinations(lst, r)) for r in range(len(lst)+1))
+#     # print(*allSums)
+#     for i in allSums:
+#         print(*i, end='\n')
+#
+#     #     sumlst.append(i)
+#     # print(*sumlst)
+#
+#         # if lstlen <= len(lst):
+#         #     sumFromListCombination(lstlen)
+#         #     lstlen += 1
+#         # else:
+#         #     break
+#
+# sumFromListCombination()
+
+
 import itertools
 
+banknotes = [10, 50, 100, 200, 500, 1000, 2000, 5000]
+sums = []
 
-def sumFromListCombination():
-    lst = [10, 50, 100, 200, 500, 1000, 2000, 5000]
-    # lstlen = len(lst)
-    sumlst = []
-    allSums = itertools.chain((itertools.combinations(lst, r)) for r in range(len(lst)+1))
-    # print(*allSums)
-    for i in allSums:
-        print(*i, end='\n')
+for j in range(1, len(banknotes) + 1):
+    #    for i in itertools.combinations([1,2,3,4,5], j):
+    for i in itertools.combinations(banknotes, j):
+        #print(i, '|', sum(i))
+        x = sums.append(sum(i))
+        #print(x)
 
-    #     sumlst.append(i)
-    # print(*sumlst)
+sums_uniq = set(sums)
+print(sorted(sums_uniq))
 
-        # if lstlen <= len(lst):
-        #     sumFromListCombination(lstlen)
-        #     lstlen += 1
-        # else:
-        #     break
-
-sumFromListCombination()
