78a83295375d8417ef58f3092638fab27a7253b1
p.teperik <p.teperik@yandex.ru>
2024-03-27 13:17:25 +0300

    Урок 29

--- task_29/task_29_1.py
+++ task_29/task_29_1.py
@@ -1,11 +1,12 @@
-def uNums(lst):
-    unique = 0
-    for num in lst:
-        unique ^= num
-    return unique
+from collections import Counter
+# lst = [2, 2, 2, 2, 7, 2, 2]
+lst = [7, 2, 7, 7]
 
-# Пример использования
-lst = [2, 2, 2, 2, 7, 2, 2]
+res =Counter(lst)
 
-res = uNums(lst)
-print(res)
+def get_key(res, value):
+    for k, v in res.items():
+        if v == value:
+            return k
+
+print(get_key(res, 1))
